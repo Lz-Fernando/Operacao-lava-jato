c8be3272003c5f70bcfbc110c776c5e12e4d24d6
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-05-13 10:21:36 -0300

    Criação dos arquivos

---
+++ functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "structs.h"
+
+void menu() {
+    printf("1. Add a new student\n");
+    printf("2. Add a new course\n");
+    printf("3. Add a new grade\n");
+    printf("4. Show all students\n");
+    printf("5. Show all courses\n");
+    printf("6. Show all grades\n");
+    printf("7. Show all students with their courses and grades\n");
+    printf("8. Exit\n");
+}
---
+++ structs.h
@@ -0,0 +1,11 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+void menu();
+
+#endif // STRUCTS_H
